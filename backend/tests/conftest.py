d94e6a1a7723b83231b7bebc68da95d1dc6168b4
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-02-24 19:50:45 -0500

    Add a second test

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,4 +1,10 @@
+from enum import Enum
 from fastapi.testclient import TestClient
 from src.main import app
 
 client = TestClient(app)
+
+def encoder_for_enums(obj):
+    if isinstance(obj, Enum):
+        return obj.value
+    return obj
--- backend/tests/test_questions.py
+++ backend/tests/test_questions.py
@@ -1,17 +1,33 @@
-from .conftest import client
+from .conftest import client, encoder_for_enums
 from src.models import QuestionType, Choice, Answer
+from src.routes.questions.add_question import QuestionRequest
 
 class TestQuestions:
     def test_add_multiple_choice_question(self):
         question_text = "Sample **Question**?"
         choices = [Choice(text="Choice 1", choice_id=1), Choice(text="Choice 2", choice_id=2), Choice(text="Choice 3", choice_id=3), Choice(text="Choice 4", choice_id=4)]
         answer = Answer(correct_choice_ids=[3])
-        question_data = {"type": QuestionType.multiple_choice.value, "question_text": question_text, "choices": [model.dict() for model in choices], "answer": answer.dict()}
-        response = client.post("/question", json=question_data)
-        json_response = response.json()
+        question_data = QuestionRequest(type=QuestionType.multiple_choice, question_text=question_text, choices=choices, answer=answer)
+        response = client.post("/question", content=question_data.json(encoder=encoder_for_enums), headers={"Content-Type": "application/json"})
         assert response.status_code == 200
+        json_response = response.json()
         assert json_response["type"] == QuestionType.multiple_choice.value
         assert json_response["question_text"] == question_text
         assert json_response["choices"] == [model.dict() for model in choices]
         assert json_response["answer"] == answer
         assert json_response["id"]
+
+    def test_short_answer_question(self):
+        question_text = "Sample **Question**?"
+        answer = Answer(text="Answer")
+        question_data = QuestionRequest(type=QuestionType.short_answer, question_text=question_text, choices=None, answer=answer)
+        response = client.post("/question", content=question_data.json(encoder=encoder_for_enums), headers={"Content-Type": "application/json"})
+        assert response.status_code == 200
+        json_response = response.json()
+        assert json_response["type"] == QuestionType.short_answer.value
+        assert json_response["question_text"] == question_text
+        assert json_response["choices"] is None
+        assert json_response["answer"] == answer
+        assert json_response["id"]
+
+    
