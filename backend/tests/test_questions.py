6a16be3f62122b4f4b19181344ae2d23bacf0702
wrighs6 <wrighs6@rpi.edu>
2023-02-28 16:35:14 -0500

    added test for adding well-formed select_one

--- backend/tests/test_questions.py
+++ backend/tests/test_questions.py
@@ -43,4 +43,17 @@ class TestQuestions:
         assert json_response["choices"] == [model.dict() for model in choices]
         assert json_response["answer"] == answer
         assert json_response["id"]
-    
+
+    def test_add_select_one_question(self):
+        question_text = "Sample **Question**?"
+        choices = [Choice(text="Choice 1", choice_id=1), Choice(text="Choice 2", choice_id=2), Choice(text="Choice 3", choice_id=3), Choice(text="Choice 4", choice_id=4)]
+        answer = Answer(correct_choice_ids=[1,3])
+        question_data = QuestionRequest(type=QuestionType.select_one, question_text=question_text, choices=choices, answer=answer)
+        response = client.post("/question", content=question_data.json(encoder=encoder_for_enums), headers={"Content-Type": "application/json"})
+        assert response.status_code == 200
+        json_response = response.json()
+        assert json_response["type"] == QuestionType.select_one.value
+        assert json_response["question_text"] == question_text
+        assert json_response["choices"] == [model.dict() for model in choices]
+        assert json_response["answer"] == answer
+        assert json_response["id"]
