d5a548d8837c5f054f16df9debf0d371b6b409eb
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-03-03 17:20:55 -0500

    Start work on login component

---
+++ frontend/src/components/Login/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+  
+	function doRedirect(){
+    window.location.href = "http://localhost:8080/login";
+  }
+</script>
+
+<template>
+  
+  <button id="login-button" @click="doRedirect()">Login!</button>
+
+</template>
+
+<script>
+
+</script>
