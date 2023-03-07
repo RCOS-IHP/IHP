26949cd7e3f5197c3af4ebb7ea1c6962d59bb414
Andy Chen <112647105+andy12q4@users.noreply.github.com>
2023-03-06 22:55:04 -0500

    New components

---
+++ frontend/Homepage - Vue/src/components/Table.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-table theme="dark">
+    <thead>
+      <tr>
+        <th class="text-left">
+          Name
+        </th>
+        <th class="text-left">
+          Calories
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="item in desserts"
+        :key="item.name"
+      >
+        <td>{{ item.name }}</td>
+        <td>{{ item.calories }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
