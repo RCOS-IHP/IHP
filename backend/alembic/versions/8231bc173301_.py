6e3f4f7f9f1a230465f987aab16f386a60cf785b
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-03-21 19:30:54 -0400

    Do first migration

---
+++ backend/alembic/versions/8231bc173301_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8231bc173301
+Revises: 
+Create Date: 2023-03-21 17:35:14.772657
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8231bc173301'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
