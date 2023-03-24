92c125c37e24df000ee60a9ecefd76a6826ef7bb
wrighs6 <wrighs6@rpi.edu>
2023-03-24 16:37:41 -0400

    added testing

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -1,11 +1,13 @@
 from .root import root
 from .questions.add_question import add_question
+from .questions.remove_question import remove_question
 from .questions.get_question import get_question
 from .questions.list_questions import list_questions
 
 routes = [
     root,
     add_question,
+    remove_question,
     get_question,
     list_questions,
 ]
--- backend/src/routes/questions/remove_question.py
+++ backend/src/routes/questions/remove_question.py
@@ -3,7 +3,7 @@ from ...models import Question
 from ...main import app
 
 @app.delete("/question/{question_id}", status_code=204)
-async def delete_question(question_id: int):
+async def remove_question(question_id: int):
     question = await Question.objects.get_or_none(id=question_id)
     if question is None:
         raise HTTPException(status_code=404, detail="Question not found")
