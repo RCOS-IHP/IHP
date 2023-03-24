ce1f19cec491cf7ec2436578426cb83282c57193
wrighs6 <wrighs6@rpi.edu>
2023-03-24 16:37:41 -0400

    added testing

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -1,5 +1,6 @@
 from .root import root
 from .questions.add_question import add_question
+from .questions.remove_question import remove_question
 from .questions.get_question import get_question
 from .questions.list_questions import list_questions
 from .auth.login import login
@@ -8,6 +9,7 @@ from .auth.signup import signup
 routes = [
     root,
     add_question,
+    remove_question,
     get_question,
     list_questions,
     login,
