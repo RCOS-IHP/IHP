a191513577442e3e355e1308654f1d76da01d6fe
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-03-28 19:47:42 -0400

    Get docker working

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,9 +1,9 @@
-from .constants import PROJECT_ROOT
+from .constants import BACKEND_ROOT
 from .utils import display_fatal_error
 from yaml import safe_load
 from typing import TypedDict
 
-config_path = PROJECT_ROOT / "config.yml"
+config_path = BACKEND_ROOT / "config.yml"
 
 if not config_path.exists():
     display_fatal_error(f"Config file does not exist! Make sure {config_path.absolute()} exists.")
--- backend/src/main.py
+++ backend/src/main.py
@@ -12,20 +12,12 @@ database = databases.Database(config['db'])
 app.state.database = database
 add_pagination(app)
 
-async def make_tables():
-    import sqlalchemy
-    # get your database url in sqlalchemy format - same as used with databases instance used in Model definition
-    engine = sqlalchemy.create_engine(config['db'])
-    # note that this has to be the same metadata that is used in ormar Models definition
-    metadata.create_all(engine)
-
 
 @app.on_event("startup")
 async def startup() -> None:
     database_ = app.state.database
     if not database_.is_connected:
         await database_.connect()
-    await make_tables()
     
 
 
