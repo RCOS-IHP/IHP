829477bbfe3c9ce248b686d3969664010b8727fe
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-04-14 16:52:47 -0400

    Fix list questions

--- backend/src/routes/questions/list_questions.py
+++ backend/src/routes/questions/list_questions.py
@@ -1,9 +1,6 @@
 from ...models import Question
 from ...main import app
 
-from fastapi_pagination import Page
-from fastapi_pagination.ext.ormar import paginate
-
-@app.get("/question", response_model=Page[Question])
+@app.get("/question", response_model=list[Question])
 async def list_questions():
-    return await paginate(Question.objects)
+    return await Question.objects.all()
