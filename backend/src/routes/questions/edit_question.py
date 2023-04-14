777d58250b0e08303f1f2083937f1086ea7d1bc8
wrighs6 <wrighs6@rpi.edu>
2023-04-14 16:23:04 -0400

    fixed edit_question

--- backend/src/routes/questions/edit_question.py
+++ backend/src/routes/questions/edit_question.py
@@ -1,8 +1,11 @@
-from fastapi import HTTPException
+from typing import Annotated
+
+from fastapi import HTTPException, Header
 from ...models import Choice, QuestionType, Question, Answer
 from ...main import app
 from pydantic import BaseModel
 from ...utils import convert_to_json
+from ..auth.auth_common import get_user_or_401
 
 class QuestionRequest(BaseModel):
     type: QuestionType
@@ -11,14 +14,16 @@ class QuestionRequest(BaseModel):
     answer: Answer
 
 @app.post("/question/{question_id}", response_model=Question)
-async def edit_question(question_id:int, question: QuestionRequest):
+async def edit_question(authorization: Annotated[str, Header()], question_id:int, question: QuestionRequest):
     """This handles the request to edit questions in the database"""
+    user = await get_user_or_401(authorization)
     question_model = Question(
         id=question_id,
         type=question.type.value,
         question_text=question.question_text,
         choices=convert_to_json(question.choices),
-        answer=convert_to_json(question.answer)
+        answer=convert_to_json(question.answer),
+        creator=user
     )
 
     if question.type is QuestionType.short_answer:
@@ -36,6 +41,6 @@ async def edit_question(question_id:int, question: QuestionRequest):
             if len(question.answer.correct_choice_ids) == 1:
                 raise HTTPException(status_code=400, detail="For this question type, there must be multiple correct answers")
 
-    await question_model.save()
+    await question_model.update()
     return question_model
 
