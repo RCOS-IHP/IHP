d01dabe2148156b1e94f95c9313a424aef20f2cd
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-04-14 17:02:19 -0400

    Add test for get and list questions

--- backend/tests/test_questions.py
+++ backend/tests/test_questions.py
@@ -1,3 +1,4 @@
+from random import randint
 from .conftest import client, encoder_for_enums
 from src.models import QuestionType, Choice, Answer
 from src.routes.questions.add_question import QuestionRequest
@@ -162,4 +163,47 @@ class TestQuestions:
         assert json_edit_response["question_text"] == new_question_text
         assert json_edit_response["answer"] == new_answer
         assert json_edit_response["id"] == id
-        
+    
+    def test_get_question(self):
+        self.saturate_login()
+        random_test_txt = str(randint(0, 1000000))
+        answer = Answer(text="Answer")
+        question_data = QuestionRequest(type=QuestionType.short_answer, question_text=random_test_txt, choices=None, answer=answer)
+        response = client.post("/question", content=question_data.json(encoder=encoder_for_enums), headers={"Content-Type": "application/json", "Authorization": f"Bearer {TestQuestions.login_cached_access_token}"})
+        assert response.status_code == 200
+        json_response = response.json()
+        assert json_response["type"] == QuestionType.short_answer.value
+        assert json_response["question_text"] == random_test_txt
+        assert json_response["choices"] is None
+        assert json_response["answer"] == answer
+        assert json_response["id"]
+        id = json_response["id"]
+        response = client.get(f"/question/{id}")
+        assert response.status_code == 200
+        json_response = response.json()
+        assert json_response["type"] == QuestionType.short_answer.value
+        assert json_response["question_text"] == random_test_txt
+        assert json_response["choices"] is None
+        assert json_response["answer"] == answer
+        assert json_response["id"] == id
+
+    def test_list_questions(self):
+        self.saturate_login()
+        random_test_txt = str(randint(0, 1000000))
+        answer = Answer(text="Answer")
+        question_data = QuestionRequest(type=QuestionType.short_answer, question_text=random_test_txt, choices=None, answer=answer)
+        response = client.post("/question", content=question_data.json(encoder=encoder_for_enums), headers={"Content-Type": "application/json", "Authorization": f"Bearer {TestQuestions.login_cached_access_token}"})
+        assert response.status_code == 200
+        json_response = response.json()
+        assert json_response["type"] == QuestionType.short_answer.value
+        assert json_response["question_text"] == random_test_txt
+        assert json_response["choices"] is None
+        assert json_response["answer"] == answer
+        assert json_response["id"]
+        id = json_response["id"]
+        response = client.get("/question")
+        assert response.status_code == 200
+        json_response = response.json()
+        assert type(json_response) == list
+        assert len(json_response) >= 1
+        assert any([x["id"] == id for x in json_response])
